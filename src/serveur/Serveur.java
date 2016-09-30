4814158ede237edecfc4d2db3962806d73c23130
Geoffrey DESBROSSES <E15A413E@S046V7pc05.ensinfo.sciences.univ-nantes.prive>
2016-09-30 14:36:30 +0200

    correction bug serveur

--- src/serveur/Serveur.java
+++ src/serveur/Serveur.java
@@ -1,60 +1,60 @@
 package serveur;
 
-import java.net.InetAddress;
 import java.net.MalformedURLException;
-import java.net.UnknownHostException;
+import java.rmi.AlreadyBoundException;
 import java.rmi.Naming;
 import java.rmi.RMISecurityManager;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
+import java.util.ArrayList;
+
+import client.Acheteur;
+
+
 
 
 public class Serveur{
 
-	
+	private final static int port = 8090;
 
 	public static void main(String[] argv) {
+		Thread t = new Thread();
+		Donnees bdd = new Donnees();
+		bdd.initObjets();
 		
 		
 		try {
-			
+			VenteImpl vente = new VenteImpl(new ArrayList<Acheteur>(), bdd.getListeObjets().get(0));
+			LocateRegistry.createRegistry(port);
+	
+			/*
 			//Securite pour l acces distant
-			LocateRegistry.createRegistry(8090);		 
 			if (System.getSecurityManager() == null) {
 				System.setSecurityManager(new RMISecurityManager());
 			}
+			*/
 			
-			//Creation de l objet distant
-			VenteImpl vente = new VenteImpl();
-			String url = "rmi://" + InetAddress.getLocalHost().getHostAddress() + "/Enchere";
-			Naming.rebind(url, vente);
-			//Naming.bind("//localhost:8090/enchere", vente); // //host:port/name
+		Naming.bind("//localhost:"+port+"/enchere", vente); // //host:port/name
 			
+
+	
+		while(true){
 			
-			
-			Thread t = new Thread();
-			
-			while(true){
-				
-				
-				
-				t.sleep(2000);
-				System.out.println(vente.getEtatVente());
-				System.out.println(vente.getObjet().getPrixCourant());
-			
-			}
-			
-				
+			t.sleep(2000);
+			System.out.println(vente.getEtatVente());
+			System.out.println(vente.getObjet().getPrixCourant());
+		
+		}
 			
 		} catch (RemoteException e) {
 			e.printStackTrace();
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
-		}
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (AlreadyBoundException e) {
+			e.printStackTrace();
+		} 
 		
 		
 	}	
--- src/serveur/Vente.java
+++ src/serveur/Vente.java
@@ -31,5 +31,7 @@ public interface Vente extends Remote {
 	 * @throws RemoteException
 	 */
 	public int tempsEcoule(Acheteur acheteur) throws RemoteException, Exception;
+	
+	public Objet getObjet();
 
 }
